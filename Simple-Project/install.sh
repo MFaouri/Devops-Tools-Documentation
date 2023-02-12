c9ac4c7291d3cdd92ef3873697ee23a161c01d50
mfaouri <mohanadfaouri@gmail.com>
2023-02-12 21:09:28 +0300

    fixing script

--- Simple-Project/install.sh
+++ Simple-Project/install.sh
@@ -8,10 +8,20 @@ echo "--------------------Installing Jenkins--------------------"
 sudo apt -y install wget
 sudo wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
 sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee \
+    /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+    https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+    /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update
+sudo apt-get install fontconfig openjdk-11-jre
+
 sudo apt-get update -y
 sudo apt-get install jenkins -y
 sudo apt install git
 sudo systemctl start jenkins
+
 #Install docker
 echo "--------------------Installing Docker--------------------"
 sudo apt-get update -y \
